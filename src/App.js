ee81ea1aeab476a4da79356a5d2519ec767c11c1
Muslim Ibrahim <imuslim615@gmail.com>
2025-05-05 11:49:25 +0100

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <main style={{ textAlign: 'center', marginTop: '20vh' }}>
+      <h1>Hello World</h1>
+    </main>
+  );
+}
+
+export default App;
+
